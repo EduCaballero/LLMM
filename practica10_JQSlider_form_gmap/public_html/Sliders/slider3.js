61413d8c3ba4b57e7ade1d6c54a541a8201c0c18
EduStrife <edustrife@gmail.com>
2017-03-23 21:07:16 +0100

    +

--- practica10_JQSlider_form_gmap/public_html/Sliders/slider3.js
+++ practica10_JQSlider_form_gmap/public_html/Sliders/slider3.js
@@ -1,16 +1,33 @@
 $(document).ready(ini);
 function ini() {
-    $('.single-item').slick({
-        autoplay: true,
-        dots:true
-    });
-}
-
-$(document).ready(ini);
-    function ini() {
-    $('.multiple-items').slick({
-      infinite: true,
-      slidesToShow: 2,
-      autoplay:true  
+    $('.slider').slick({
+        prevArrow:"<img class='a-left control-c prev slick-prev' src='al.jpg'>",
+        nextArrow:"<img class='a-right control-c next slick-next' src='ar.jpg'>",
+        dots: true,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        responsive: [
+        {
+          breakpoint: 850,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 700,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
     });
 }
